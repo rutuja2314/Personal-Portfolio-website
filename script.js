f08e0ad3abcfeb27457f030822eb205f88e51bfc
rutuja2314 <rutujasawant743@gmail.com>
2023-12-28 15:41:26 +0530

    changes done

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ window.onload = function() {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
+  
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
